edc30b23e9f82a49a488e558666e4f13b5552c1d
Robert Gifford <robertgif@gmail.com>
2018-01-09 17:11:21 -0500

    Update build process to force i386 xcompilation
    * TODO:
    *	Create and push a new netbsd patch

--- global.mk
+++ global.mk
@@ -4,7 +4,7 @@ CFLAGS+= -std=gnu99 ${DBG}
 CFLAGS+= -fno-stack-protector -ffreestanding
 CXXFLAGS+= -fno-stack-protector -ffreestanding
 
-CFLAGS+= -Wall -Wimplicit -Wmissing-prototypes -Wstrict-prototypes -Wl,-r
+CFLAGS+= -Wall -Wimplicit -Wmissing-prototypes -Wstrict-prototypes -Wl,-r -Wl,-melf_i386 -m32
 ifndef NOGCCERROR
 CFLAGS+= -Werror
 endif
